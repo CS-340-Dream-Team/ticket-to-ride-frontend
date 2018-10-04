e535edf37e85a6f337c150231eea2822756dda0d
Andrew Tate <drewctate@gmail.com>
2018-10-04 15:55:17 -0600

    Fixing prod build

--- src/app/pages/login-screen/login-screen.component.ts
+++ src/app/pages/login-screen/login-screen.component.ts
@@ -8,6 +8,9 @@ import { Router } from '@angular/router';
 })
 export class LoginScreenComponent implements OnInit {
 
+  // UI Flag
+  public showRegister = false;
+
   constructor(private router: Router) { }
 
   ngOnInit() {
