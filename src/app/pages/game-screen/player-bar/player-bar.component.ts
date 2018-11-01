803172469c9a3210d607bdd0a525d8f47306a6e9
Andrew Tate <drewctate@gmail.com>
2018-10-31 21:27:04 -0600

    Adding card counts and route list for client player

--- src/app/pages/game-screen/player-bar/player-bar.component.ts
+++ src/app/pages/game-screen/player-bar/player-bar.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Player, PlayerColor } from 'src/app/types';
+import {
+  BusCard,
+  BusColor,
+  Player,
+  PlayerColor,
+  Route
+} from 'src/app/types';
 import { GamePlayManagerService } from 'src/app/services';
 
 @Component({
@@ -7,11 +13,25 @@ import { GamePlayManagerService } from 'src/app/services';
   templateUrl: './player-bar.component.html',
   styleUrls: ['./player-bar.component.scss']
 })
-export class PlayerBarComponent implements OnInit {
+export class PlayerBarComponent {
 
   public playerColor = PlayerColor;
 
   public players: Player[] = [];
+
+  public busCardCounts = {
+    red: 0,
+    orange: 0,
+    yellow: 0,
+    green: 0,
+    blue: 0,
+    purple: 0,
+    black: 0,
+    white: 0,
+    rainbow: 0
+  };
+
+  public routeCards: Route[];
 
   constructor(
     private gamePlayManager: GamePlayManagerService
@@ -19,11 +39,31 @@ export class PlayerBarComponent implements OnInit {
     this.gamePlayManager.allPlayersSubject.subscribe({
       next: players => {
         this.players = players;
+
+        const clientPlayerArr = this.players.filter(player => this.isCurrentPlayer(player));
+        if (clientPlayerArr.length) {
+          const clientPlayer = clientPlayerArr[0];
+          this.routeCards = <Route[]>clientPlayer.routeCards;
+          this.countBusCards(<BusCard[]>clientPlayer.busCards);
+        }
       }
     });
   }
 
-  ngOnInit() {
+  /**
+   * Counts the number of each type bus card
+   * @param clientPlayer the client player
+   */
+  private countBusCards(busCards: BusCard[]) {
+    this.busCardCounts.red = busCards.filter(card => card.color === BusColor.Red).length;
+    this.busCardCounts.orange = busCards.filter(card => card.color === BusColor.Orange).length;
+    this.busCardCounts.yellow = busCards.filter(card => card.color === BusColor.Yellow).length;
+    this.busCardCounts.green = busCards.filter(card => card.color === BusColor.Green).length;
+    this.busCardCounts.blue = busCards.filter(card => card.color === BusColor.Blue).length;
+    this.busCardCounts.purple = busCards.filter(card => card.color === BusColor.Purple).length;
+    this.busCardCounts.black = busCards.filter(card => card.color === BusColor.Black).length;
+    this.busCardCounts.white = busCards.filter(card => card.color === BusColor.White).length;
+    this.busCardCounts.rainbow = busCards.filter(card => card.color === BusColor.Rainbow).length;
   }
 
   public isCurrentPlayer(player) {
