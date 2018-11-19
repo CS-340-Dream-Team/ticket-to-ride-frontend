297494f63126a5eb78724314d9d0c6f90ceced98
Andrew Tate <drewctate@gmail.com>
2018-11-19 15:48:48 -0700

    Claim segment component

---
+++ src/app/pages/game-play/claim-segment/claim-segment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-claim-segment',
+  templateUrl: './claim-segment.component.html',
+  styleUrls: ['./claim-segment.component.scss']
+})
+export class ClaimSegmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
