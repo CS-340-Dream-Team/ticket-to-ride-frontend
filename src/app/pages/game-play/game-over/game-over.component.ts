ece17084233c127be908b5d02324293bd9c5a13e
michaelsimmons94 <michaelsimmons94@gmail.com>
2018-11-19 23:28:54 -0700

    fixed starting a second game

--- src/app/pages/game-play/game-over/game-over.component.ts
+++ src/app/pages/game-play/game-over/game-over.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GameOverStat } from 'src/app/types/game-over-stat/GameOverStat';
 import { GamePlayManagerService, GameListManagerService } from 'src/app/services';
 import { Router } from '@angular/router';
+import { ChatManagerService } from '../../../services/chat-manager/chat-manager.service';
 
 @Component({
   selector: 'app-game-over',
@@ -11,7 +12,7 @@ import { Router } from '@angular/router';
 export class GameOverComponent implements OnInit {
   _stats: GameOverStat[] = [];
 
-  constructor(private gamePlayManagerService: GamePlayManagerService, private router: Router, private glms: GameListManagerService) {
+  constructor(private gamePlayManagerService: GamePlayManagerService, private router: Router, private glms: GameListManagerService, private cms: ChatManagerService) {
     gamePlayManagerService.gameOverStatsSubject.subscribe({
       next: (stats) => this._stats = stats
     })
@@ -23,5 +24,8 @@ export class GameOverComponent implements OnInit {
   _exit(): void {
     this.router.navigate(['/game-list']);
     this.glms.setCurrentGame(null);
+    this.cms.stopPolling();
+    this.glms.startPolling();
+    
   }
 }
