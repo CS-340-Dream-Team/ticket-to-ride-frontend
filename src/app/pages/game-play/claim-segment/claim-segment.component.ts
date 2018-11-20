36bf642d855c13d3eb932d8d5e15c46d62fcfe61
Carter McBride <junk@carter.works>
2018-11-19 23:39:39 -0700

    Basic styling for the claim segment modal

--- src/app/pages/game-play/claim-segment/claim-segment.component.ts
+++ src/app/pages/game-play/claim-segment/claim-segment.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Segment, BusColor } from 'src/app/types';
+
+
 
 @Component({
   selector: 'app-claim-segment',
@@ -7,7 +10,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClaimSegmentComponent implements OnInit {
 
-  constructor() { }
+
+  regularColorCount: number = 0;
+  wildColorCount: number = 0;
+  selectedColor: BusColor = BusColor.Red;
+
+  readonly segment: Segment = {
+    id: 1,
+    start: {
+      name: 'Baseball Stadium',
+      latLong: {
+        lat: 40.254821,
+        long: -111.651125
+      }
+    },
+    end: {
+      name: 'LaVell Edwards Stadium',
+      latLong: {
+        lat: 40.257536,
+        long: -111.654664
+      }
+    },
+    length: 1,
+    pair: 2,
+    color: BusColor.Rainbow
+  }
+
+  get segmentColor(): string {
+    return BusColor[this.segment.color];
+  }
+
+  get selectedColorStr(): string {
+    return BusColor[this.selectedColor];
+  }
+
+  set selectedColorStr(value: string) {
+    console.log(value);
+    this.selectedColor = BusColor[value];
+  }
+
+  get playerHasWilds(): boolean {
+    return true;
+  }
+
+  get cardDelta(): number {
+    return Math.abs(this.cardsLeftToSet);
+  }
+
+  get cardsLeftToSet(): number {
+    return this.regularColorCount + this.wildColorCount - this.segment.length;
+  }
+
+  get needFewerCards(): boolean {
+    return this.cardsLeftToSet > 0;
+  }
+
+  get isValid(): boolean {
+    return this.regularColorCount + this.wildColorCount === this.segment.length;
+  }
+
+  constructor() {
+    this.regularColorCount = this.segment.length;
+    this.wildColorCount = 0;
+    this.selectedColor = this.segment.color === BusColor.Rainbow ? BusColor.Red : this.segment.color;
+   }
 
   ngOnInit() {
   }
